1f2413a2726f9e842bb540854106e713150fecc4
Sunkovpe <dexinzeta@gmail.com>
2025-11-21 23:39:15 -0500

    fix

--- plugins/main-creador.js
+++ plugins/main-creador.js
@@ -1,6 +1,7 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-  let vcard1 = `BEGIN:VCARD\nVERSION:3.0\nN:;Sung;;\nFN:Sung\nORG:\nTITLE:\nitem1.TEL;waid=51942501966:51942501966\nitem1.X-ABLabel:\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:\nEND:VCARD`
-  let vcard2 = `BEGIN:VCARD\nVERSION:3.0\nN:;Sunkovv;;\nFN:Sunkovv\nORG:\nTITLE:\nitem1.TEL;waid=51901437507:51901437507\nitem1.X-ABLabel:\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:\nEND:VCARD`
+  let vcard1 = `BEGIN:VCARD\nVERSION:3.0\nN:;Sunkovv </>;;\nFN:Sunkovv </>\nORG:\nTITLE:\nitem1.TEL;waid=51901437507:5901437507\nitem1.X-ABLabel:\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:\nEND:VCARD`
+  let vcard2 = `BEGIN:VCARD\nVERSION:3.0\nN:;Fernando;;\nFN:Fernando\nORG:\nTITLE:\nitem1.TEL;waid=5218448582376:5218448582376\nitem1.X-ABLabel:\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:\nEND:VCARD`
+  let vcard3 = `BEGIN:VCARD\nVERSION:3.0\nN:;Kevin;;\nFN:Kevin\nORG:\nTITLE:\nitem1.TEL;waid=5219881032907:5219881032907\nitem1.X-ABLabel:\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:\nEND:VCARD`
   await conn.sendMessage(m.chat, {
     contacts: {
       displayName: 'Contactos',
